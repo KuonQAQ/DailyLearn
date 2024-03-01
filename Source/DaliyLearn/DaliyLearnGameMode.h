d6b79073520a475fa0fd35f6fb74bd60931f7d51
ly <478208485@qq.com>
2024-03-01 16:32:58 +0800

    init

---
+++ Source/DaliyLearn/DaliyLearnGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DaliyLearnGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADaliyLearnGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADaliyLearnGameMode();
+};
+
+
+
